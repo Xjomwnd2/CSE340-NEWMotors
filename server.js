4086ad44d324de9fe84e84c7d8f1a79b0a021a69
Joel Ndiba Mwaura <mwa21001@byui.edu>
2024-10-20 21:54:42 +0300

    COMMIT

--- server.js
+++ server.js
@@ -29,26 +29,3 @@ app.use(static);
 app.get("/", function(req, res){
   res.render("index", {title: "Home"});
 });
-
-
-
-/* ************************************************
-Validation Middleware
-************************************************* */
-
-
-
-
-/* ***********************
- * Local Server Information
- * Values from .env (environment) file
- *************************/
-const port = process.env.PORT
-const host = process.env.HOST
-
-/* ***********************
- * Log statement to confirm server operation
- *************************/
-app.listen(port, () => {
-  console.log(`app listening on ${host}:${port}`);
-});
