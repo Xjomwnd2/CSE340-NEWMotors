f2a9cf45204b9ca8549e6847fb2d33079029d160
Joel Ndiba Mwaura <mwa21001@byui.edu>
2024-10-20 03:54:48 +0300

    commit

--- server.js
+++ server.js
@@ -5,16 +5,20 @@
 /* ***********************
  * Require Statements
  *************************/
-const express = require("express")
-const env = require("dotenv").config()
-const app = express()
-const static = require("./routes/static")
+const express = require("express");
+const env = require("dotenv").config();
+const app = express();
+const static = require("./routes/static");
 
 /* ***********************
  * Routes
  *************************/
-app.use(static)
+app.use(static);
 
+// Index route
+app.get("/", function(req, res){
+  res.render("index", {title: "Home"});
+});
 /* ***********************
  * Local Server Information
  * Values from .env (environment) file
@@ -26,5 +30,5 @@ const host = process.env.HOST
  * Log statement to confirm server operation
  *************************/
 app.listen(port, () => {
-  console.log(`app listening on ${host}:${port}`)
-})
+  console.log(`app listening on ${host}:${port}`);
+});
